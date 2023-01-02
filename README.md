4825a66c81d8efbd89e1da7c82e14686f729b1f6
bntoine <87795477+bntoine@users.noreply.github.com>
2023-01-02 15:29:29 +0100

    Initial commit 2 electric boogaloo

--- README.md
+++ README.md
@@ -1,4 +1,10 @@
 # esp-Vibe
 **WIP**
 
-We vibin.
+Most of this code was taken from https://github.com/MonomoriumP/Buttplug.io--Lelo
+
+I have tested this with xtoys. It should work with buttplugio but I haven't tested.
+
+For now it emulates a lovense edge and gives you two channels (on pins 13 and 14 by default) with 20 steps.
+
+You should be able to use https://stpihkal.docs.buttplug.io/docs/stpihkal/protocols/lovense/ to emulate a different toy from lovense.
--- src/main.cpp
+++ src/main.cpp
@@ -1,189 +1,183 @@
-/*
-WIP
+/**
 
-Eventually should be able to receive a list of arguments over tcp to make a series of patterns as well as arbitrary values for the motor in real time.
+Most of this code was taken from https://github.com/MonomoriumP/Buttplug.io--Lelo
+I have tested this with xtoys.app, it should work with buttplug.io but I haven't tested
+For now it emulates a lovense edge and gives you two channels with 20 steps.
+You should be able to use https://stpihkal.docs.buttplug.io/docs/stpihkal/protocols/lovense/ to emulate a different toy from lovense.
 
-Potential inspiration
-https://github.com/jcfain/TCodeESP32
+**/
 
-*/
 
 #include <Arduino.h>
-#include <ESP8266WiFi.h>
-#include <WiFiClient.h>
-#include <vector>
+//#include <analogWrite.h> // The ESP32 analogWrite library by ERROPiX is needed if you are using the arduino IDE
+#include <BLEDevice.h>
+#include <BLEUtils.h>
+#include <BLEServer.h>
+#include <BLE2902.h>
 
-  // Wifi credentials
-  const char* ssid = "espVibe";
-  const char* password = "bruhmoment";
 
-  // Motor pin. (must be PWM capable)
-  int mot = 2;
-  int btn = D7;
+//          ================= Motor pins ================
+uint8_t mtr1 = 13;
+uint8_t mtr2 = 14;
 
-  // Number of steps to increase or decrease i by per loop. (influences the effect of speed setting)
-  uint8_t step = 3;
-  
-  // Pseudo loop variable for patterns.
-  uint8_t i;
 
-  // Timekeeping for the loops.
-  unsigned int oldMillis = 0;
-  unsigned int currentMillis;
-  unsigned int sTime;
+BLEServer* pServer = NULL;
+BLECharacteristic* pTxCharacteristic = NULL;
+BLECharacteristic* pRxCharacteristic = NULL;
+bool deviceConnected = false;
+bool oldDeviceConnected = false;
+uint32_t value = 0;
 
-  unsigned short int interval;
-  
-  
-  // Getting around the problem of not using loops with flags.
-  bool doOnce =false;
+// Only vibration1 and vibration2 are used here but I assume vibration is useful in vibrators with a single motor.
 
-  // Wifi and TCP variables
+int vibration;
+int vibration1;
+int vibration2;
 
-  WiFiServer server(5045);
-  WiFiClient client = server.available();
-  IPAddress local_IP(192,168,4,1);
-
-/*
-Vibration stuff
-*/
+#define SERVICE_UUID           "50300001-0023-4bd4-bbd5-a6920e4c5653"
+#define CHARACTERISTIC_RX_UUID "50300002-0023-4bd4-bbd5-a6920e4c5653"
+#define CHARACTERISTIC_TX_UUID "50300003-0023-4bd4-bbd5-a6920e4c5653"
 
 
 
-// Breathing pattern going between sStrength and eStrength for duration waiting 100/speed ms between each step.
-void breathing(uint8_t sStrength, uint8_t eStrength, unsigned int duration, unsigned short int speed) {
+void SetMTR(void) {
+  int power1 = map(vibration1, 0 , 20 , 0, 255);
+  power1 = constrain(power1, 0, 255);
+  analogWrite(mtr1, power1);
 
-  if (!doOnce) {
-    doOnce = true;
-    interval = 1000/constrain(speed, 1, 65535);
-    i = sStrength+step;
-    sTime = currentMillis;
-  }
+  int power2 = map(vibration2, 0 , 20 , 0, 255);
+  power2 = constrain(power2, 0, 255);
+  analogWrite(mtr2, power2);
+}
 
-  if (currentMillis - sTime <= duration){
-    if (currentMillis - oldMillis >= interval){
 
-        oldMillis = currentMillis;
-        i += step;
-        if(i >= eStrength || i <= sStrength) {step = -step;}
-        analogWrite(mot, i);
 
+class MyServerCallbacks: public BLEServerCallbacks {
+    void onConnect(BLEServer* pServer) {
+      deviceConnected = true;
+      BLEDevice::startAdvertising();
+    };
+
+    void onDisconnect(BLEServer* pServer) {
+      deviceConnected = false;
     }
-  }
-
-  else {
-      digitalWrite(mot, LOW);
-      doOnce = false;
-  }
-}
-
-// Run at strength for time
-
-void singleShot(uint8_t strength, unsigned int duration) {
-
-  if (!doOnce) {
-    doOnce = true;
-    sTime = currentMillis;
-    analogWrite(mot, strength);
-  }
-  if (currentMillis - sTime >= duration || duration > 0){ // If duration = 0 real time mode just continue until next input
-    doOnce = false;
-    digitalWrite(mot, LOW);
-  }
+};
 
 
-  
-}
 
-// Function to create a square pattern going between sStrength and eStrength for duration ms alternating at 1/speed hz with T_sStrength/T_eStrength = 1/(ratio-1).
-void pulsing(uint8_t sStrength, uint8_t eStrength, unsigned int duration, unsigned short int speed, uint8_t ratio = 2) {
+class MySerialCallbacks: public BLECharacteristicCallbacks {
+    void onWrite(BLECharacteristic *pCharacteristic) {
+      static uint8_t messageBuf[64];
+      assert(pCharacteristic == pRxCharacteristic);
+      std::string rxValue = pRxCharacteristic->getValue();
 
-  if (!doOnce) {
-    doOnce = true;
-    interval = 1000/constrain(speed, 1, 65535);
-    sTime = currentMillis;
-  }
-
-  if (currentMillis - sTime <= duration){
-    if (currentMillis - oldMillis >= interval - interval/ratio){
-      if (currentMillis - oldMillis >= interval){
-          oldMillis = currentMillis;
-          analogWrite(mot, eStrength);
+      
+      if (rxValue == "DeviceType;") {
+        memmove(messageBuf, "P:37:FFFFFFFFFFFF;", 18);
+        // CONFIGURATION:               ^ Use a BLE address of the Lovense device you're cloning. (Doesn't seem to matter for xtoys. Might need to add radomization to avoid conflicts but I need to check with a second board I don't have yet.)
+        pTxCharacteristic->setValue(messageBuf, 18);
+        pTxCharacteristic->notify();
+      } else if (rxValue == "Battery;") {
+        memmove(messageBuf, "69;", 3);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+      } else if (rxValue == "PowerOff;") {
+        memmove(messageBuf, "OK;", 3);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+      } else if (rxValue == "RotateChange;") {
+        memmove(messageBuf, "OK;", 3);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+      } else if (rxValue.rfind("Status:", 0) == 0) {
+        memmove(messageBuf, "2;", 2);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+      } else if (rxValue.rfind("Vibrate:", 0) == 0) {
+        vibration = std::atoi(rxValue.substr(8).c_str());
+        memmove(messageBuf, "OK;", 3);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+        SetMTR();
+      } else if (rxValue.rfind("Vibrate1:", 0) == 0) {
+        vibration1 = std::atoi(rxValue.substr(9).c_str());
+        memmove(messageBuf, "OK;", 3);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+        SetMTR();
+      } else if (rxValue.rfind("Vibrate2:", 0) == 0) {
+        vibration2 = std::atoi(rxValue.substr(9).c_str());
+        memmove(messageBuf, "OK;", 3);
+        pTxCharacteristic->setValue(messageBuf, 3);
+        pTxCharacteristic->notify();
+        SetMTR();
+      } else {
+        memmove(messageBuf, "ERR;", 4);
+        pTxCharacteristic->setValue(messageBuf, 4);
+        pTxCharacteristic->notify();
       }
-        
-      else {
-          analogWrite(mot, sStrength);
-      }
     }
-  }
-  else {
-    digitalWrite(mot, LOW);
-    doOnce = false;
-  }
-
-
-}
-
-// Handle deserialization for the tcp interface.
-struct PatternParams {
-    uint8_t mode;
-    uint8_t sStrength;
-    uint8_t eStrength;
-    uint32_t duration;
-    uint16_t speed;
-    uint8_t ratio;
-  };
-
-  char serialize(PatternParams* params) {
-    return params->mode | params->sStrength << 8 | params->eStrength << 16 | params->duration << 24 | params->speed << 56 | params->ratio << 72;
-  }
-
-  PatternParams deserialize(char data) {
-    PatternParams params{};
-    params.mode = data & 0xFF; // First byte
-    params.sStrength = data & 0xFF; // Next byte
-    params.eStrength = data >> 8 & 0xFF; // Next byte
-    params.duration = data >> 16 & 0xFFFFFFFF; // Next 4 bytes
-    params.speed = data >> 48 & 0xFFFF; // Next 2 bytes
-    params.ratio = data >> 64 & 0xFF; // Last byte
-    return params;
-}
-
-
-void TCPServer () {
-  if (client) {
-    if (client.connected()) {
-      Serial.println("Connected to client");   
-    }
-    if (client.available() > 0) {
-      char data = client.read();
-      PatternParams params;
-      params = deserialize(data);
-      server.write("It veurks wirelessly!\n");
-      Serial.write(data);
-      Serial.write("\n");
-      Serial.write(params.mode);
-    }
-  }
-}
-
-// Function to handle the queuing of patterns and order of execution.
-void pattern() {
-
-}
+};
 
 void setup() {
-  Serial.begin(115200);
-  Serial.println("It veurks!");
-  pinMode(mot, OUTPUT);
-  pinMode(btn, INPUT);
-  WiFi.softAP(ssid,password);
-  server.begin();
+  
+  
+  pinMode(mtr1, OUTPUT);
+  pinMode(mtr2, OUTPUT);
+  delay(100);
+  digitalWrite(mtr1, LOW);
+  delay(100);
+  analogWrite(mtr1, 127);
+  delay(100);
+  digitalWrite(mtr1, LOW);
+  delay(100);
+ 
+  
+  // Create the BLE Device
+  BLEDevice::init("LVS-EDGE"); // CONFIGURATION: The name doesn't actually matter, The app identifies it by the reported id.
+
+  // Create the BLE Server
+  pServer = BLEDevice::createServer();
+  pServer->setCallbacks(new MyServerCallbacks());
+
+  // Create the BLE Service
+  BLEService *pService = pServer->createService(SERVICE_UUID);
+
+  // Create a BLE Characteristics
+  pTxCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_TX_UUID,
+                      BLECharacteristic::PROPERTY_NOTIFY
+                    );
+  pTxCharacteristic->addDescriptor(new BLE2902());
+
+  pRxCharacteristic = pService->createCharacteristic(
+                      CHARACTERISTIC_RX_UUID,
+                      BLECharacteristic::PROPERTY_WRITE  |
+                      BLECharacteristic::PROPERTY_WRITE_NR
+                    );
+  pRxCharacteristic->setCallbacks(new MySerialCallbacks());
+
+  // Start the service
+  pService->start();
+
+  // Start advertising
+  BLEAdvertising *pAdvertising = BLEDevice::getAdvertising();
+  pAdvertising->addServiceUUID(SERVICE_UUID);
+  pAdvertising->setScanResponse(false);
+  pAdvertising->setMinPreferred(0x0);  // set value to 0x00 to not advertise this parameter
+  BLEDevice::startAdvertising();
 }
 
 void loop() {
-  currentMillis = millis();
-  TCPServer();
-  
-
+    // disconnecting
+    if (!deviceConnected && oldDeviceConnected) {
+        delay(100); // give the bluetooth stack the chance to get things ready
+        pServer->startAdvertising(); // restart advertising
+        oldDeviceConnected = deviceConnected;
+    }
+    // connecting
+    if (deviceConnected && !oldDeviceConnected) {
+        // do stuff here on connecting
+        oldDeviceConnected = deviceConnected;
+    }
 }
